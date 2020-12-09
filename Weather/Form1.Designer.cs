c55e825dff5c869a7f895c589a6e437776c8aebd
BloodDi84 <74744815+BloodDi84@users.noreply.github.com>
2020-12-09 18:31:03 +0300

    Add files via upload

---
+++ Weather/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Weather
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LCity = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LTemperature = new System.Windows.Forms.Label();
+            this.PBWeather = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PBWeather)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(202, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Город";
+            // 
+            // LCity
+            // 
+            this.LCity.AutoSize = true;
+            this.LCity.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LCity.Location = new System.Drawing.Point(365, 96);
+            this.LCity.Name = "LCity";
+            this.LCity.Size = new System.Drawing.Size(53, 21);
+            this.LCity.TabIndex = 2;
+            this.LCity.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(202, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Температура";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(63, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 21);
+            this.label5.TabIndex = 1;
+            // 
+            // LTemperature
+            // 
+            this.LTemperature.AutoSize = true;
+            this.LTemperature.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LTemperature.Location = new System.Drawing.Point(365, 126);
+            this.LTemperature.Name = "LTemperature";
+            this.LTemperature.Size = new System.Drawing.Size(53, 21);
+            this.LTemperature.TabIndex = 2;
+            this.LTemperature.Text = "label2";
+            // 
+            // PBWeather
+            // 
+            this.PBWeather.Image = global::Weather.Properties.Resources._50n;
+            this.PBWeather.Location = new System.Drawing.Point(286, 197);
+            this.PBWeather.Name = "PBWeather";
+            this.PBWeather.Size = new System.Drawing.Size(100, 50);
+            this.PBWeather.TabIndex = 3;
+            this.PBWeather.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Пекин",
+            "Донецк",
+            "Макеевка",
+            "Киев",
+            "Харьков",
+            "Красный Луч"});
+            this.comboBox1.Location = new System.Drawing.Point(240, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 29);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.LTemperature);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.PBWeather);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.LCity);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(650, 368);
+            this.panel1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(240, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 58);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Weather.Properties.Resources._000;
+            this.ClientSize = new System.Drawing.Size(650, 368);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Name = "Form";
+            this.Text = "Погода от Брежнева";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PBWeather)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LCity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LTemperature;
+        private System.Windows.Forms.PictureBox PBWeather;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Weather/Form1.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weather
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+        string c = "Pekin";
+
+        public void Weather()
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={c}&appid=9748a46a0ba3134b4670fead1f1c43df&units=metric";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string response = reader.ReadToEnd();
+            WeatherResponse wr = JsonConvert.DeserializeObject<WeatherResponse>(response);
+            LCity.Text = wr.Name;
+            LTemperature.Text = wr.Main.Temp.ToString();
+        }
+ 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            Weather();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch(comboBox1.SelectedIndex) {
+
+                case 0:
+                    c = "Donetsk";
+                break;
+                case 1:
+                    c = "Makiyivka";
+                    break;
+                case 2:
+                    c = "Kyiv";
+                    break;
+                case 3:
+                    c = "Kharkiv";
+                    break;
+                case 4:
+                    c = "Krasnyy Luch";
+                    break;
+
+                    
+
+            }
+            Weather();
+        }
+    }
+}
